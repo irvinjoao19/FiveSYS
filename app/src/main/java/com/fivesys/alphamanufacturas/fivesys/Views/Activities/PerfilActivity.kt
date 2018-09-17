c13c4aa368198fde7973fa16e2510c3484724cc1
irvinjoao19 <gongorairvin@gmail.com>
2018-09-17 16:04:55 -0500

    IRVIN

--- app/src/main/java/com/fivesys/alphamanufacturas/fivesys/Views/Activities/PerfilActivity.kt
+++ app/src/main/java/com/fivesys/alphamanufacturas/fivesys/Views/Activities/PerfilActivity.kt
@@ -2,12 +2,30 @@ package com.fivesys.alphamanufacturas.fivesys.Views.Activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.TabLayout
+import android.support.v7.app.ActionBar
+import android.support.v7.widget.Toolbar
 import com.fivesys.alphamanufacturas.fivesys.R
+import java.util.*
 
 class PerfilActivity : AppCompatActivity() {
+
+    lateinit var toolbar: Toolbar
+    lateinit var tabLayout: TabLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_perfil)
+        bindToolbar()
+    }
+
+    private fun bindToolbar() {
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        Objects.requireNonNull<ActionBar>(supportActionBar).title = "Perfil"
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        toolbar.setNavigationOnClickListener {
+            finish()
+        }
     }
 }
