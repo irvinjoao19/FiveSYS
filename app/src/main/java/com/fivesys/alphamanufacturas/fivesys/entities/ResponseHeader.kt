2fb2f158eee30d58ddf0a786fb7c1465fb26de90
= <=>
2018-10-14 16:00:38 -0500

    add icon filter

--- app/src/main/java/com/fivesys/alphamanufacturas/fivesys/entities/ResponseHeader.kt
+++ app/src/main/java/com/fivesys/alphamanufacturas/fivesys/entities/ResponseHeader.kt
@@ -2,17 +2,19 @@ package com.fivesys.alphamanufacturas.fivesys.entities
 
 import io.realm.RealmList
 import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
 
 open class ResponseHeader : RealmObject {
 
-    var mensaje: String? = ""
-    var codigo: Int? = 0
+    @PrimaryKey
     var id: Int? = 0
+    var mensaje: String? = ""
+    var codigo: String? = ""
     var puntosFijos: RealmList<PuntosFijosHeader>? = null
 
     constructor() : super()
 
-    constructor(mensaje: String?, codigo: Int?, id: Int?, puntosFijos: RealmList<PuntosFijosHeader>?) : super() {
+    constructor(mensaje: String?, codigo: String?, id: Int?, puntosFijos: RealmList<PuntosFijosHeader>?) : super() {
         this.mensaje = mensaje
         this.codigo = codigo
         this.id = id
