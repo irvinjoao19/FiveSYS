381a4e4f1fbc2f6e962bef00c7424c5e8d235e20
irvinjoao19 <irvin.dsige@gmail.com>
2019-02-11 18:06:29 -0500

    irvin changes work

---
+++ app/src/main/java/com/fivesys/alphamanufacturas/fivesys/views/activities/RegisterActivity.kt
@@ -0,0 +1,13 @@
+package com.fivesys.alphamanufacturas.fivesys.views.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fivesys.alphamanufacturas.fivesys.R
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
